2460e900e08b1e9578caf790da54d4ee92fafe5f
Cameron Messinides <cameron.messinides@gmail.com>
2019-08-18 21:49:26 -0400

    chore(org): scope all Python scripts to a dedicated directory

--- scripts/lib/settings.py
+++ scripts/lib/settings.py
@@ -4,7 +4,7 @@ from logging import ERROR
 
 from dotenv import load_dotenv
 
-_env_path = Path('..') / '.env'
+_env_path = Path(os.path.dirname(__file__)) / '..' / '.env'
 load_dotenv(dotenv_path=_env_path)
 
 REDDIT_USERNAME = os.getenv("REDDIT_USERNAME")
@@ -12,4 +12,4 @@ REDDIT_PASSWORD = os.getenv("REDDIT_PASSWORD")
 REDDIT_CLIENT_ID = os.getenv("REDDIT_CLIENT_ID")
 REDDIT_CLIENT_SECRET = os.getenv("REDDIT_CLIENT_SECRET")
 LOG_LEVEL = int(os.getenv("LOG_LEVEL", ERROR))
-DATA_DIR = Path(os.path.dirname(__file__)) / '..' / 'data'
+DATA_DIR = Path(os.path.dirname(__file__)) / '..' / '..' / 'data'
