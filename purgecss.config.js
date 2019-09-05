23bcc510c3fc90312b4b808866f44153ca537e37
Cameron Messinides <cameron.messinides@gmail.com>
2019-09-05 14:57:55 -0400

    feat(content): explain sentiment analysis with comment examples

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-	content: ["**/*.pug", "./src/*.jsx"]
+	content: ["**/*.pug", "./src/*.jsx"],
+	defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+	whitelist: [
+		// To keep normalize styles in, even if they don't appear in the markup
+		"blockquote",
+		"dl",
+		"dd",
+		"h1",
+		"h2",
+		"h3",
+		"h4",
+		"h5",
+		"h6",
+		"hr",
+		"figure",
+		"p",
+		"pre",
+		"ol",
+		"ul"
+	]
 };
