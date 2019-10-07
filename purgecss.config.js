1f97fcde10015114a6f63b95e70d49e80136e224
Cameron Messinides <cameron.messinides@gmail.com>
2019-10-07 12:14:45 -0400

    fix(viz): exclude data table styles from CSS purging

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,22 +1,24 @@
 module.exports = {
-	content: ["**/*.pug", "./src/*.jsx"],
-	defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-	whitelist: [
-		// To keep normalize styles in, even if they don't appear in the markup
-		"blockquote",
-		"dl",
-		"dd",
-		"h1",
-		"h2",
-		"h3",
-		"h4",
-		"h5",
-		"h6",
-		"hr",
-		"figure",
-		"p",
-		"pre",
-		"ol",
-		"ul"
-	]
+  content: ["**/*.pug", "./src/*.jsx"],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  whitelist: [
+    // To keep normalize styles in, even if they don't appear in the markup
+    "blockquote",
+    "dl",
+    "dd",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "hr",
+    "figure",
+    "p",
+    "pre",
+    "ol",
+    "ul",
+    "text-right"
+  ],
+  whitelistPatterns: [/^c-(data-)?table__.+/]
 };
